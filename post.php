b9f3548929e274c1db7ef04136de0d6ba90a4330
pavelp21 <pavelvp2182@yandex.ru>
2018-08-01 17:36:28 +0500

    Typeste and responsive are done

---
+++ post.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, minimum-scale=1, maximum-scale=1"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <meta name="author" content="Master Vision"/>
+    <meta name="description" content="Ваша ссылка для доступа на вебинар &amp;laquo;Анатомия отношений&amp;raquo;"/>
+    <title>Ваша ссылка для доступа на вебинар "Анатомия отношений"</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.png"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/styles.css"/>
+  </head>
+  <body id="link"><!-- Yandex.Metrika counter -->
+    <script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter35450015 = new Ya.Metrika({ id:35450015, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script>
+    <noscript>
+      <div><img src="https://mc.yandex.ru/watch/35450015" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript><!-- /Yandex.Metrika counter -->
+    <div id="main_screen">
+      <div class="container">
+        <h1>Ваша ссылка для доступа на вебинар:</h1><a class="link_for" href="#!" target="_blank">http://start.bizon365.ru/room/6538/trans7</a>
+        <p class="begin_date">Начало в <span>15:15 </span>- через <span>13 </span>минут</p><img src="img/small_ball.png" alt="Маленький шарик-декор" id="small_ball_post"/>
+      </div>
+    </div>
+    <script src="js/libs.min.js"></script>
+    <script src="js/link_scripts.js"></script>
+  </body>
+</html>
